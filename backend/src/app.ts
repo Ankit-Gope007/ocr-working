238ba9cbd82311c559935d28d8314ed5a6571466
Ankit Gope <ankitgope21@gmail.com>
2025-09-29 18:01:21 +0530

    Update CORS configuration to allow specific origin
    Restrict CORS to a specific origin for security.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,11 @@ import ocrRoutes from "./routes/ocrRoute";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+  origin: "https://ocr-working-6udoddpxr-ankit-gopes-projects-893eb2f8.vercel.app",
+}
+));
 app.use(express.json());
 
 // Routes
